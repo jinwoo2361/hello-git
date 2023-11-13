25c9e1b249c2792da08e5fd05a01efceaf2e8747
jinwoo2361 <jinwoo236161@gmail.com>
2023-11-13 11:06:18 +0900

    Lion#punch 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -2,6 +2,7 @@ package com.sh.app;
 
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
+import com.sh.app.animal.Lion;
 
 public class Main {
 
@@ -10,6 +11,7 @@ public class Main {
 		System.out.println("Hello Git");
 		new Dog().bark();
 		new Cat().jump();
+		new Lion().punch();
 	}
 
 }
---
+++ src/com/sh/app/animal/Lion.java
@@ -0,0 +1,9 @@
+package com.sh.app.animal;
+
+public class Lion {
+
+	public void punch() {
+		System.out.println("펀치");
+	}
+
+}
