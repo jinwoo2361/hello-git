65335744756537df2ab7d3368105dcfebbf13be7
jinwoo2361 <jinwoo236161@gmail.com>
2023-11-13 10:33:38 +0900

    Dog#bark 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,10 +1,13 @@
 package com.sh.app;
 
+import com.sh.app.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Git");
+		new Dog().bark();
 	}
 
 }
