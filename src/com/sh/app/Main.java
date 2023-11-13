ad7fdf4dada809584abdd5623f1e8507d2604511
jinwoo2361 <jinwoo236161@gmail.com>
2023-11-13 11:26:54 +0900

    Bird#fly 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,5 +1,6 @@
 package com.sh.app;
 
+import com.sh.app.animal.Bird;
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
 import com.sh.app.animal.Lion;
@@ -12,6 +13,7 @@ public class Main {
 		new Dog().bark();
 		new Cat().jump();
 		new Lion().punch();
+		new Bird().fly();
 	}
 
 }
---
+++ src/com/sh/app/animal/Bird.java
@@ -0,0 +1,10 @@
+package com.sh.app.animal;
+
+public class Bird {
+
+	public void fly() {
+
+		System.out.println("새 날기");
+	}
+
+}
